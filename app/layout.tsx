75c7f542f7ee2f142f32c594a7aacd36b28e3c54
mxwnk <mx.wink@gmail.com>
2025-04-09 23:03:01 +0200

    Add new target icon as play button and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body style={{ margin: 0 }}>
         {children}
         <SpeedInsights />
--- app/play-button.tsx
+++ app/play-button.tsx
@@ -1,12 +1,14 @@
 'use client';
-import { Disc } from 'lucide-react';
 import Link from 'next/link';
+import { TargetIcon } from './assets/target-icon';
 
 export function PlayButton() {
   return (
-    <div className="pointer w-[400px] rounded-2xl p-4 shadow-2xl bg-primary text-center">
+    <div className="pointer w-[350px] rounded-2xl p-4 shadow-2xl bg-primary text-center">
       <Link className="flex flex-col items-center" href="/play">
-        <Disc className="w-[80%] h-[80%] text-white" />
+        <div className="w-[200px] h-[200px]">
+          <TargetIcon />
+        </div>
         <h3 className="text-5xl text-white">Play</h3>
       </Link>
     </div>

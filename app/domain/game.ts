c39e472961610469a1d94734b6d93a9ff7b390e3
mxwnk <mx.wink@gmail.com>
2025-04-15 22:31:24 +0200

    Force double out throw

--- app/domain/game.ts
+++ app/domain/game.ts
@@ -1,147 +1,173 @@
-import { dartThrownEvent, DartThrown, DomainEvent, GameCreated, gameCreatedEvent, PlayerAdded, playerAddedEvent } from "./events";
-import { calcScoreOfTurns, findNextPlayer, Turn } from '../models/turn';
-import { calcScore, Dart } from '../models/dart';
-import { Player, PlayerWithPositon } from '../models/player';
-import { Checkout } from '../models/checkout';
-import { createGameId } from '../models/id';
+import {
+  dartThrownEvent,
+  DartThrown,
+  DomainEvent,
+  GameCreated,
+  gameCreatedEvent,
+  PlayerAdded,
+  playerAddedEvent,
+} from "./events";
+import { calcScoreOfTurns, findNextPlayer, Turn } from "../models/turn";
+import { calcScore, Dart } from "../models/dart";
+import { Player, PlayerWithPositon } from "../models/player";
+import { Checkout } from "../models/checkout";
+import { createGameId } from "../models/id";
 
 export class Game {
-    private id: string;
-    private events: DomainEvent[] = [];
-    private players: PlayerWithPositon[] = [];
-    private startpoints!: number;
-    private checkout!: Checkout;
-    private currentPlayer!: PlayerWithPositon;
-    private turns: Map<string, Turn[]> = new Map();
+  private id: string;
+  private events: DomainEvent[] = [];
+  private players: PlayerWithPositon[] = [];
+  private startpoints!: number;
+  private checkout!: Checkout;
+  private currentPlayer!: PlayerWithPositon;
+  private turns: Map<string, Turn[]> = new Map();
 
-    public static start(setup?: { startpoints?: number, checkout?: Checkout }) {
-        const gameCreated = gameCreatedEvent({
-            gameId: createGameId(),
-            startpoints: setup?.startpoints ?? 301,
-            checkout: setup?.checkout ?? "Straight"
-        });
-        return new Game([gameCreated]);
+  public static start(setup?: { startpoints?: number; checkout?: Checkout }) {
+    const gameCreated = gameCreatedEvent({
+      gameId: createGameId(),
+      startpoints: setup?.startpoints ?? 301,
+      checkout: setup?.checkout ?? "Straight",
+    });
+    return new Game([gameCreated]);
+  }
+
+  public static from(events: DomainEvent[]) {
+    const game = new Game(events);
+    game.flush();
+    return game;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  private constructor(events: DomainEvent[]) {
+    const id = events[0].gameId;
+    this.id = id;
+    for (const event of events) {
+      this.apply(event);
+    }
+  }
+
+  public addPlayer(player: Player) {
+    const gameId = this.id;
+    const position = this.players.length;
+    const playerAdded = playerAddedEvent({
+      gameId,
+      payload: { id: player.id, name: player.name, position },
+    });
+    this.apply(playerAdded);
+  }
+
+  public throwDart(dart: Dart) {
+    if (this.isGameOver()) {
+      return;
+    }
+    const overthrown = this.isWrongCheckout(dart);
+    const dartThrown = dartThrownEvent({
+      gameId: this.id,
+      payload: {
+        playerId: this.currentPlayer.id,
+        overthrown,
+        ...dart,
+      },
+    });
+    this.apply(dartThrown);
+  }
+
+  public flush(): DomainEvent[] {
+    const newEvents = JSON.parse(JSON.stringify(this.events));
+    this.events = [];
+    return newEvents;
+  }
+
+  private isWrongCheckout(dartThrow: Dart) {
+    const missingScore = this.calculateMissingScore(this.currentPlayer.id);
+    const currentScore = calcScore(dartThrow);
+    const newMissingScore = missingScore - currentScore;
+    if (newMissingScore < 0) {
+      return true;
+    }
+    if (
+      this.checkout == "Double" &&
+      (newMissingScore === 1 || newMissingScore === 0)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  private hasPlayerWon(playerId: string) {
+    return this.calculateMissingScore(playerId) === 0;
+  }
+
+  private calculateMissingScore(playerId: string) {
+    const playerTurns = this.turns.get(playerId);
+    if (!playerTurns) {
+      return this.startpoints;
+    }
+    return this.startpoints - calcScoreOfTurns(playerTurns);
+  }
+
+  private isGameOver() {
+    return this.players.every((p) => this.hasPlayerWon(p.id));
+  }
+
+  private apply(event: DomainEvent) {
+    /** @ts-ignore */
+    this[`apply${event.type}`](event);
+    this.events.push(event);
+  }
+
+  private applyGameCreated(event: GameCreated) {
+    this.id = event.gameId;
+    this.startpoints = event.payload.startpoints;
+    this.checkout = event.payload.checkout;
+  }
+
+  private applyPlayerAdded(event: PlayerAdded) {
+    if (this.players.length === 0) {
+      this.currentPlayer = event.payload;
+    }
+    this.players.push(event.payload);
+  }
+
+  private applyDartThrown(event: DartThrown) {
+    const { playerId, score, ring, overthrown } = event.payload;
+
+    if (!this.turns.has(playerId)) {
+      this.turns.set(playerId, []);
     }
 
-    public static from(events: DomainEvent[]) {
-        const game = new Game(events);
-        game.flush();
-        return game;
+    const playerTurns = this.turns.get(playerId)!;
+
+    if (
+      playerTurns.length === 0 ||
+      playerTurns.at(-1)!.darts.length >= 3 ||
+      playerTurns.at(-1)?.overthrown
+    ) {
+      playerTurns.push({ darts: [], overthrown: false });
     }
 
-    public getId(): string {
-        return this.id;
+    const currentTurn = playerTurns.at(-1)!;
+    currentTurn.darts.push({ score, ring });
+    currentTurn.overthrown = overthrown;
+
+    const isTurnOver =
+      currentTurn.darts.length === 3 ||
+      currentTurn.overthrown ||
+      this.hasPlayerWon(playerId);
+    if (!isTurnOver) {
+      return;
     }
-
-    private constructor(events: DomainEvent[]) {
-        const id = events[0].gameId;
-        this.id = id;
-        for (const event of events) {
-            this.apply(event);
-        }
+    const nextPlayer = findNextPlayer({
+      playerTurns: this.turns,
+      currentPlayer: this.currentPlayer,
+      startpoints: this.startpoints,
+      players: this.players,
+    });
+    if (nextPlayer) {
+      this.currentPlayer = nextPlayer;
     }
-
-    public addPlayer(player: Player) {
-        const gameId = this.id;
-        const position = this.players.length;
-        const playerAdded = playerAddedEvent({ gameId, payload: { id: player.id, name: player.name, position } });
-        this.apply(playerAdded);
-    }
-
-    public throwDart(dart: Dart) {
-        if (this.isGameOver()) {
-            return;
-        }
-        const overthrown = this.isWrongCheckout(dart);
-        const dartThrown = dartThrownEvent({
-            gameId: this.id, payload: {
-                playerId: this.currentPlayer.id,
-                overthrown,
-                ...dart
-            }
-        });
-        this.apply(dartThrown);
-    }
-
-    public flush(): DomainEvent[] {
-        const newEvents = JSON.parse(JSON.stringify(this.events));
-        this.events = [];
-        return newEvents;
-    }
-
-    private isWrongCheckout(dartThrow: Dart) {
-        const missingScore = this.calculateMissingScore(this.currentPlayer.id);
-        const currentScore = calcScore(dartThrow);
-        const newMissingScore = missingScore - currentScore;
-        if (newMissingScore < 0) {
-            return true;
-        }
-        if (this.checkout == "Double" && newMissingScore === 1) {
-            return true;
-        }
-        return false;
-    }
-
-    private hasPlayerWon(playerId: string) {
-        return this.calculateMissingScore(playerId) === 0;
-    }
-
-    private calculateMissingScore(playerId: string) {
-        const playerTurns = this.turns.get(playerId);
-        if (!playerTurns) {
-            return this.startpoints;
-        }
-        return this.startpoints - calcScoreOfTurns(playerTurns);
-    }
-
-    private isGameOver() {
-        return this.players.every(p => this.hasPlayerWon(p.id));
-    }
-
-    private apply(event: DomainEvent) {
-        /** @ts-ignore */
-        this[`apply${event.type}`](event);
-        this.events.push(event);
-    }
-
-    private applyGameCreated(event: GameCreated) {
-        this.id = event.gameId;
-        this.startpoints = event.payload.startpoints;
-        this.checkout = event.payload.checkout;
-    }
-
-    private applyPlayerAdded(event: PlayerAdded) {
-        if (this.players.length === 0) {
-            this.currentPlayer = event.payload;
-        }
-        this.players.push(event.payload);
-    }
-
-    private applyDartThrown(event: DartThrown) {
-        const { playerId, score, ring, overthrown } = event.payload;
-
-        if (!this.turns.has(playerId)) {
-            this.turns.set(playerId, []);
-        }
-
-        const playerTurns = this.turns.get(playerId)!;
-
-
-        if (playerTurns.length === 0 || playerTurns.at(-1)!.darts.length >= 3 || playerTurns.at(-1)?.overthrown) {
-            playerTurns.push({ darts: [], overthrown: false });
-        }
-
-        const currentTurn = playerTurns.at(-1)!;
-        currentTurn.darts.push({ score, ring });
-        currentTurn.overthrown = overthrown;
-
-        const isTurnOver = currentTurn.darts.length === 3 || currentTurn.overthrown || this.hasPlayerWon(playerId);
-        if (!isTurnOver) {
-            return;
-        }
-        const nextPlayer = findNextPlayer({ playerTurns: this.turns, currentPlayer: this.currentPlayer, startpoints: this.startpoints, players: this.players });
-        if (nextPlayer) {
-            this.currentPlayer = nextPlayer;
-        }
-    }
+  }
 }
--- tests/domain/game.test.ts
+++ tests/domain/game.test.ts
@@ -1,99 +1,112 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 import { Game } from "@/app/domain/game";
-import { seedPlayer } from '../seeder/player.seed';
-import { DartThrown, PlayerAdded } from '@/app/domain/events';
-import { Checkout } from '@/app/models/checkout';
+import { seedPlayer } from "../seeder/player.seed";
+import { DartThrown, PlayerAdded } from "@/app/domain/events";
+import { Checkout } from "@/app/models/checkout";
 
 describe("Game", () => {
-    it("should allow to create a new game", async () => {
-        const game = Game.start();
+  it("should allow to create a new game", async () => {
+    const game = Game.start();
 
-        const events = game.flush();
-        expect(events.length).toBe(1);
-        expect(events[0].type = "GameCreated");
+    const events = game.flush();
+    expect(events.length).toBe(1);
+    expect((events[0].type = "GameCreated"));
+  });
+
+  it("should allow to add player", async () => {
+    const game = Game.start();
+    const player = seedPlayer();
+
+    game.addPlayer(player);
+
+    const events = game.flush();
+    const playerAdded = events[1] as PlayerAdded;
+    expect(playerAdded.type).toBe("PlayerAdded");
+    expect(playerAdded.payload.id).toBe(player.id);
+    expect(playerAdded.payload.name).toBe(player.name);
+  });
+
+  it("should allow to add multiple players", async () => {
+    const game = Game.start();
+    const first = seedPlayer();
+    const second = seedPlayer();
+
+    game.addPlayer(first);
+    game.addPlayer(second);
+
+    const events = game.flush();
+    const playerAddedEvents = events.slice(1, 3);
+    expect(playerAddedEvents.length).toBe(2);
+    playerAddedEvents.every((e) => expect(e.type).toBe("PlayerAdded"));
+  });
+
+  describe("Darts", () => {
+    it("should track thrown darts", async () => {
+      const game = seedGame();
+
+      game.throwDart({ score: 1, ring: "D" });
+
+      const events = game.flush();
+      expect(events.length).toBe(1);
+      var dartThrownEvent = events[0] as DartThrown;
+      expect(dartThrownEvent.type).toBe("DartThrown");
+      expect(dartThrownEvent.payload.score).toBe(1);
+      expect(dartThrownEvent.payload.ring).toBe("D");
     });
 
-    it("should allow to add player", async () => {
-        const game = Game.start();
-        const player = seedPlayer();
+    it("should mark throw as overthrown if score < 0", async () => {
+      const game = seedGame({ startpoints: 2 });
 
-        game.addPlayer(player);
+      game.throwDart({ score: 3, ring: "D" });
 
-        const events = game.flush();
-        const playerAdded = events[1] as PlayerAdded;
-        expect(playerAdded.type).toBe("PlayerAdded");
-        expect(playerAdded.payload.id).toBe(player.id);
-        expect(playerAdded.payload.name).toBe(player.name);
+      const events = game.flush();
+      expect(events.length).toBe(1);
+      var dartThrownEvent = events[0] as DartThrown;
+      expect(dartThrownEvent.type).toBe("DartThrown");
+      expect(dartThrownEvent.payload.overthrown).toBe(true);
     });
 
-    it("should allow to add multiple players", async () => {
-        const game = Game.start();
-        const first = seedPlayer();
-        const second = seedPlayer();
+    it("should mark throw as legal if missing score > 0", async () => {
+      const game = seedGame({ startpoints: 2 });
 
-        game.addPlayer(first);
-        game.addPlayer(second);
+      game.throwDart({ score: 1 });
 
-        const events = game.flush();
-        const playerAddedEvents = events.slice(1, 3);
-        expect(playerAddedEvents.length).toBe(2);
-        playerAddedEvents.every(e => expect(e.type).toBe("PlayerAdded"));
+      const events = game.flush();
+      expect(events.length).toBe(1);
+      var dartThrownEvent = events[0] as DartThrown;
+      expect(dartThrownEvent.type).toBe("DartThrown");
+      expect(dartThrownEvent.payload.overthrown).toBe(false);
     });
 
-    describe("Darts", () => {
-        it("should track thrown darts", async () => {
-            const game = seedGame();
+    it("should mark throw as overthrown if double checkout busted", async () => {
+      const game = seedGame({ startpoints: 2, checkout: "Double" });
 
-            game.throwDart({ score: 1, ring: "D" })
+      game.throwDart({ score: 1, ring: undefined });
 
-            const events = game.flush();
-            expect(events.length).toBe(1);
-            var dartThrownEvent = events[0] as DartThrown;
-            expect(dartThrownEvent.type).toBe("DartThrown")
-            expect(dartThrownEvent.payload.score).toBe(1)
-            expect(dartThrownEvent.payload.ring).toBe("D");
-        });
+      const events = game.flush();
+      var dartThrownEvent = events[0] as DartThrown;
+      expect(dartThrownEvent.payload.overthrown).toBe(true);
+    });
 
-        it("should mark throw as overthrown if score < 0", async () => {
-            const game = seedGame({ startpoints: 2 });
+    it("should mark throw as overthrown if double out missed", async () => {
+      const game = seedGame({ startpoints: 2, checkout: "Double" });
 
-            game.throwDart({ score: 3, ring: "D" })
+      game.throwDart({ score: 2, ring: undefined });
 
-            const events = game.flush();
-            expect(events.length).toBe(1);
-            var dartThrownEvent = events[0] as DartThrown;
-            expect(dartThrownEvent.type).toBe("DartThrown")
-            expect(dartThrownEvent.payload.overthrown).toBe(true)
-        });
+      const events = game.flush();
+      var dartThrownEvent = events[0] as DartThrown;
+      expect(dartThrownEvent.payload.overthrown).toBe(true);
+    });
+  });
 
-        it("should mark throw as legal if missing score > 0", async () => {
-            const game = seedGame({ startpoints: 2 });
-
-            game.throwDart({ score: 1 })
-
-            const events = game.flush();
-            expect(events.length).toBe(1);
-            var dartThrownEvent = events[0] as DartThrown;
-            expect(dartThrownEvent.type).toBe("DartThrown")
-            expect(dartThrownEvent.payload.overthrown).toBe(false)
-        });
-        
-        it("should mark throw as overthrown if double checkout busted", async () => {
-            const game = seedGame({ startpoints: 2, checkout: "Double" });
-
-            game.throwDart({ score: 1, ring: undefined })
-
-            const events = game.flush();
-            var dartThrownEvent = events[0] as DartThrown;
-            expect(dartThrownEvent.payload.overthrown).toBe(true)
-        });
-    })
-
-    function seedGame(config?: { startpoints?: number, checkout?: Checkout }) {
-        const game = Game.start({ startpoints: config?.startpoints, checkout: config?.checkout });
-        const player = seedPlayer();
-        game.addPlayer(player);
-        game.flush();
-        return game;
-    }
+  function seedGame(config?: { startpoints?: number; checkout?: Checkout }) {
+    const game = Game.start({
+      startpoints: config?.startpoints,
+      checkout: config?.checkout,
+    });
+    const player = seedPlayer();
+    game.addPlayer(player);
+    game.flush();
+    return game;
+  }
 });

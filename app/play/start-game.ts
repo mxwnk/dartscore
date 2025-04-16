9e621e2b4f226eef4b497536b175dbd74cee474d
mxwnk <mx.wink@gmail.com>
2025-04-16 20:49:53 +0200

    Do not revalidate path on new game

--- app/play/start-game.ts
+++ app/play/start-game.ts
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -7,15 +7,19 @@ import { Checkout } from "../models/checkout";
 import { repository } from "../db/repository";
 
 export async function startGame(formData: FormData) {
-    const playerIds = formData.getAll("players") as string[];
-    const checkout = formData.get("checkout") as Checkout;
-    const startpoints = parseInt(formData.get("startpoints")?.toString() as string) as number;
-    const players = await prisma.player.findMany({ where: { id: { in: playerIds } } })
-    const playerMap = new Map(players.map(p => [p.id, p]));
-    const orderedPlayers = playerIds.map(id => playerMap.get(id)!);
-    const game = Game.start({ checkout, startpoints });
-    orderedPlayers.forEach((p) => game.addPlayer(p));
-    await repository.save(game);
-    revalidatePath('/setup');
-    redirect(`/play/${game.getId()}`);
+  const playerIds = formData.getAll("players") as string[];
+  const checkout = formData.get("checkout") as Checkout;
+  const startpoints = parseInt(
+    formData.get("startpoints")?.toString() as string,
+  ) as number;
+  const players = await prisma.player.findMany({
+    where: { id: { in: playerIds } },
+  });
+  const playerMap = new Map(players.map((p) => [p.id, p]));
+  const orderedPlayers = playerIds.map((id) => playerMap.get(id)!);
+  const game = Game.start({ checkout, startpoints });
+  orderedPlayers.forEach((p) => game.addPlayer(p));
+  await repository.save(game);
+  // revalidatePath('/setup');
+  redirect(`/play/${game.getId()}`);
 }

53b6e23061f49957e42be2cba1532e2cf877090e
mxwnk <mx.wink@gmail.com>
2025-10-30 10:48:52 +0100

    feat: open game details from history

--- app/components/history.tsx
+++ app/components/history.tsx
@@ -2,45 +2,44 @@ import Link from "next/link";
 import { repository } from "../db/repository";
 import { TargetIcon } from '../assets/target-icon';
 import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export const History = async () => {
     const games = await repository.getHistory();
     return (
         <Card className="inline-block mt-8">
             <h3 className="text-2xl inline-block p-4">Game History</h3>
-            <table className="table-fixed bg-popover">
-                <thead className="bg-">
-                    <tr className="[&>th]:p-4 text-xs">
-                        <th className="hidden md:table-cell"></th>
-                        <th>Points</th>
-                        <th>Checkout</th>
-                        <th>Players</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th className="hidden md:table-cell"></th>
-                    </tr>
-                </thead>
-                <tbody className="text-center">
+            <Table className="table-fixed bg-popover">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="hidden md:table-cell"></TableHead>
+                        <TableHead>Points</TableHead>
+                        <TableHead>Checkout</TableHead>
+                        <TableHead>Players</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Time</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody className="text-center">
                     {games.map(game => (
-                        <tr key={game.id}>
-                            <td className="w-16 p-4 ml-2 hidden md:table-cell"><TargetIcon /></td>
-                            <td className="text-2xl">{game.startpoints}</td>
-                            <td>{game.checkout}</td>
-                            <td className="flex flex-col p-2 text-left">
+                        <TableRow key={game.id} className="cursor-pointer">
+                            <TableCell className="w-16 p-4 ml-2 hidden md:table-cell">
+                                <TargetIcon />
+                                <Link href={`/play/${game.id}`} className="absolute inset-0 z-10" />
+                            </TableCell>
+                            <TableCell className="text-2xl">{game.startpoints}</TableCell>
+                            <TableCell>{game.checkout}</TableCell>
+                            <TableCell className="flex flex-col p-2 text-left">
                                 {game.players.map(player => (
                                     <p key={player.name}>{player.legsWon} {player.name}</p>
                                 ))}
-                            </td>
-                            <td className="px-2">{game.createdAt.toLocaleDateString("de-DE", { day: '2-digit', month: '2-digit', year: '2-digit' })}</td>
-                            <td className="px-2">{game.createdAt.toLocaleTimeString("de-DE", { hour: '2-digit', minute: '2-digit' })}</td>
-                            <td className="px-4 hidden md:table-cell">
-                                <Link className="text-primary cursor-pointer underline" href={`/play/${game.id}`}>Open</Link>
-                            </td>
-                        </tr>
+                            </TableCell>
+                            <TableCell className="px-2">{game.createdAt.toLocaleDateString("de-DE", { day: '2-digit', month: '2-digit', year: '2-digit' })}</TableCell>
+                            <TableCell className="px-2">{game.createdAt.toLocaleTimeString("de-DE", { hour: '2-digit', minute: '2-digit' })}</TableCell>
+                        </TableRow>
                     ))}
-                </tbody>
-            </table>
+                </TableBody>
+            </Table>
         </Card>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Navigation title="Dartscore" />
-      <div className="p-4 pb-12 md:p-4 flex flex-col items-center mx-auto max-w-[500px]">
+      <div className="p-4 pb-12 mb-12 md:p-4 flex flex-col items-center mx-auto max-w-[500px]">
         <PlayButton />
         <History />
       </div>
